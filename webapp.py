d4d8839b8fb9356b8904ef3e26c09b431f7c0545
emmanoe_delar <emmanoe.delar@u-bordeaux.fr>
2018-01-10 23:21:41 +0100

    premiere version rendu projet final

--- webapp.py
+++ webapp.py
@@ -8,6 +8,10 @@ import csv
 app = Flask(__name__)
 app.secret_key = 'projettechnologique'
 
+from py2neo import Graph, DBMS, authenticate
+my_dbms = DBMS("http://localhost:7474/")
+authenticate("localhost:7474", "neo4j", "3789")
+graph = Graph(password="3789")
 
 @app.route("/")
 def homepage():
@@ -18,13 +22,10 @@ def index():
     if request.method == 'POST' :
         dep = request.form['depart'];
         arr = request.form['destination'];
-        from py2neo import Graph, DBMS, authenticate
-        my_dbms = DBMS("http://localhost:7474/")
-        authenticate("localhost:7474", "neo4j", "3789")
-        graph = Graph(password="3789")
-        sec = graph.data("match (v:ville)-[r:vol_vers]->(varr:ville) where v.nom = {X} and varr.nom = {Y}  return r.distance_KM",X= dep,Y= arr)
+        dist = graph.data("match (v:ville)-[r:vol_vers]->(varr:ville) where v.nom = {X} and varr.nom = {Y}  return r.distance_KM",X= dep,Y= arr)
+        nbpass = graph.data("match (v:ville)-[r:vol_vers]->(varr:ville) where v.nom = {X} and varr.nom = {Y}  return r.nb_passagers",X= dep,Y= arr)
         simple = graph.data("match (vdep:ville)-[resc:vol_vers]->(escal:ville)-[r:vol_vers]->(varr:ville) where vdep.nom = {X} and varr.nom = {Y}  return escal, resc",X= dep,Y= arr)
-        return render_template('search.html',titre="les vols", info_vol = sec, escal = simple, dep=dep, arr=arr)
+        return render_template('search.html',titre="les vols", dist = dist, nbpass = nbpass, escal = simple, dep=dep, arr=arr)
     else:
         #A mettre dans un py 
         langDepArray=[];
@@ -35,10 +36,10 @@ def index():
 
             node_names = csv.reader(nodes, delimiter = ';')
             for vname in node_names:
-                langDepArray.append(vname[2])
-                langArrArray.append(vname[5])
-            langDepArray = sorted(set(langDepArray))
-            langArrArray = sorted(set(langArrArray))
+                langDepArray.append(vname)#5
+                langArrArray.append(vname)#2
+            #langDepArray = sorted(set(langDepArray))
+            #langArrArray = sorted(set(langArrArray))
         ####################
         # langArray = getArray()
         return render_template('form.html',  langDepArray=langDepArray, langArrArray= langArrArray)
@@ -64,21 +65,29 @@ def d3js():
 def d3s():
     return render_template('population.csv')
 
+
 ## Generate  an array with all the Countries
 with open('./templates/population.csv','r') as Countrys:
     reader = csv.reader(Countrys)
     nodes = [ n for n in Countrys][2:]
 
 country_names = csv.reader(nodes, delimiter = ',')
-country_name = []
+list_of_country = []
 for country in country_names:
-    country_name.append(country[1])
-##########################################
+    list_of_country.append(country)
+country_names = []; country_cpays = [];
 
-@app.route('/<var>')
-def tests(var):
-    if var in country_name:
-        #cpays = get_cpays(var)
-        #nb_vol = graph.data("match (vdep:ville) where vdep.codepays = {X} return count(vdep)", X = cpays)
-        return render_template('search.html')#,var=var,nb_vol=nb_vol)
+##########################################
+def get_cpays(var):
+    print(var)
+    return "CAND"
+
+@app.route('/<click_map>')
+def test(click_map):
+    for i in range(0,len(list_of_country)):
+        if click_map in list_of_country[i][1].decode('utf-8'):
+            cpays = list_of_country[i][6]
+            nb_vol_dep = graph.data("match (vdep:ville)-[r]->(varr:ville) where vdep.codepays = {X} return vdep, varr", X = cpays)
+            nb_vol_arr = graph.data("match (vdep:ville)-[r]->(varr:ville) where varr.codepays = {X} return vdep, varr", X = cpays)
+            return render_template('search.html',selected_country=click_map, dep=nb_vol_dep, arr = nb_vol_arr)
     abort(404)
