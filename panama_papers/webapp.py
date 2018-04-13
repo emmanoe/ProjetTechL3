7aeeffd185538acdfef43387dc16ee47c88be4c3
Massinissa Sadouri <msadouri@emi.u-bordeaux1.fr>
2018-04-13 16:59:41 +0200

    webapp

--- panama_papers/webapp.py
+++ panama_papers/webapp.py
@@ -13,19 +13,8 @@ app.secret_key = 'projettechnologique'
 
 from py2neo import Graph, DBMS, authenticate
 my_dbms = DBMS("http://localhost:7474/")
-authenticate("localhost:7474", "neo4j", "3789")
-graph = Graph(password="3789")
-
-## Generate an array with all the Countries used in our function
-with open('./static/population1.csv','r') as Countrys:
-    reader = csv.reader(Countrys)
-    nodes = [ n for n in Countrys][2:]
-
-country_names = csv.reader(nodes, delimiter = ',')
-list_of_country = []
-for country in country_names:
-    list_of_country.append(country)
-##########################################
+authenticate("localhost:7474", "neo4j", "pdnejoh")
+graph = Graph(password="pdnejoh")
 
 @app.route("/")
 def homepage():
@@ -34,29 +23,27 @@ def homepage():
 @app.route('/search',methods=['GET', 'POST'])
 def index():
     if request.method == 'POST' :
-        dep = request.form['depart'];
-        arr = request.form['destination'];
-        dist = graph.data("match (v:ville)-[r:vol_vers]->(varr:ville) where v.nom = {X} and varr.nom = {Y}  return r.distance_KM",X= dep,Y= arr)
-        nbpass = graph.data("match (v:ville)-[r:vol_vers]->(varr:ville) where v.nom = {X} and varr.nom = {Y}  return r.nb_passagers",X= dep,Y= arr)
-        simple = graph.data("match (vdep:ville)-[resc:vol_vers]->(escal:ville)-[r:vol_vers]->(varr:ville) where vdep.nom = {X} and varr.nom = {Y}  return escal, resc",X= dep,Y= arr)
-        return render_template('search.html',titre="les vols", dist = dist, nbpass = nbpass, escal = simple, dep=dep, arr=arr)
+        dep = request.form['recherche'];
+        print("ncouncou")
+        simple = graph.data("MATCH (n:entity) WHERE n.name CONTAINS {X} RETURN n", X=dep) #~ '.*{X}.*'
+        return render_template('search.html',titre="resultats", dep = simple)
     else:
         #A mettre dans un py
-        langPaysArray=[];
-        langVilleArray=[];
+        langDepArray=[];
+        langArrArray=[];
         with open('./static/ITA_2000.csv','r') as vol:
             reader = csv.reader(vol)
             nodes = [ n for n in vol][2:]
 
             node_names = csv.reader(nodes, delimiter = ';')
             for vname in node_names:
-                langPaysArray.append(vname)#5
-                langVilleArray.append(vname)#2
-            #langPaysArray = sorted(set(langPaysArray))
-            #langVilleArray = sorted(set(langVilleArray))
+                langDepArray.append(vname)#5
+                langArrArray.append(vname)#2
+            #langDepArray = sorted(set(langDepArray))
+            #langArrArray = sorted(set(langArrArray))
         ####################
         # langArray = getArray()
-        return render_template('form.html',  langPaysArray=list_of_country, langVilleArray= langVilleArray)
+        return render_template('form.html',  langDepArray=langDepArray, langArrArray= langArrArray)
 
 
 @app.route('/vols')
@@ -70,6 +57,18 @@ def simple():
     return render_template('vols.html',titre="les vols", ville = sec)
 
 
+## Generate  an array with all the Countries
+with open('./static/population1.csv','r') as Countrys:
+    reader = csv.reader(Countrys)
+    nodes = [ n for n in Countrys][2:]
+
+country_names = csv.reader(nodes, delimiter = ',')
+list_of_country = []
+for country in country_names:
+    list_of_country.append(country)
+country_names = []; country_cpays = [];
+
+##########################################
 
 @app.route('/<click_map>')
 def test(click_map):
@@ -78,15 +77,26 @@ def test(click_map):
             print(click_map)
             cpays = list_of_country[i][6]
             print(cpays)
-            nb_vol_dep = graph.data("match (e:entity) where e.countrycodes={X} return e", X=cpays)
+            nb_vol_dep = graph.data("match (e) where e.countrycodes={X} return e", X=cpays)
             return render_template('search.html', selected_country=click_map, dep=nb_vol_dep)
-    abort(404)
+    abort(404) ## Message d'erreur
 
 
 @app.route('/chuwebapp')
 def bddjson():
     return render_template('chuwebapp.html')
 
-@app.route('/trendline')
-def trend():
-    return render_template('trendline.html')
+
+@app.route('/graph')
+def holla():
+    from flask import request
+    from py2neo import Graph, DBMS, authenticate
+    my_dbms = DBMS("http://localhost:7474/")
+    authenticate("localhost:7474", "neo4j", "3789")
+    server = "localhost:7474"
+    graph = Graph(password="3789")
+    ent= request.args['var']
+    sec = graph.data("match(a)-[r1]->(n:entity)  where n.name={X} return r1", X=ent)
+    sec1 = graph.data("match(a)-[r1]->(n:entity)  where n.name={X} return n", X=ent)
+    sec2 = graph.data("match(a)-[r1]->(n:entity)  where n.name={X} return a", X=ent)
+    return render_template('graph.html',titre="les vols", nodes = sec, nodes1 = sec1, nodes2 = sec2 )
